9eb5be1963a26e0141e8f8e7f3dd8bae3e7f4669
Adam Ferguson <adam.ferguson06@gmail.com>
2023-04-16 12:32:49 -0700

    Adding lazy loads

--- ml_sentiment_analysis_api.py
+++ ml_sentiment_analysis_api.py
@@ -1,19 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
 
 app = FastAPI()
 
 class TextData(BaseModel):
     text: str
 
+
+def get_sentiment_analysis_pipeline():
+    tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+    model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+    sentiment_pipeline = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+    return sentiment_pipeline
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, query_param: str = None):
     return {"item_id": item_id, "query_param": query_param}
 
-@app.post("/process_text")
-def process_text(data: TextData):
-    return {"text": data.text}
+
+@app.post("/analyze_sentiment")
+def analyze_sentiment(data: TextData, sentiment_pipeline: pipeline = Depends(get_sentiment_analysis_pipeline)):
+    result = sentiment_pipeline(data.text)
+    return result
